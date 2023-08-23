ea5ff456d0996f1945cc7fd59dcdc941a1cbbf06
JJRCyber <106593073+JJRCyber@users.noreply.github.com>
2023-08-23 19:45:25 +1000

    Add Firebase

--- HomeHQ/HomeHQApp.swift
+++ HomeHQ/HomeHQApp.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct HomeHQApp: App {
+    
+    // Connect App to AppDelegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             TabBarView(showSignInView: .constant(false))
         }
+    }
+}
+
+// AppDelegate required by Firebase
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
     }
 }
