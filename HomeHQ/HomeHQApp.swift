e11b133ed3b5f4f7d2a05843b850bedf50039207
JJRCyber <106593073+JJRCyber@users.noreply.github.com>
2023-08-23 14:00:41 +1000

    Initial Commit

---
+++ HomeHQ/HomeHQApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeHQApp.swift
+//  HomeHQ
+//
+//  Created by Cooper Jacob on 23/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct HomeHQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
